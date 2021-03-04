4122e49b9578968332c34d4371d352f0539af155
Joaquín González Tesoro <65811314+JoaquinGTesoro@users.noreply.github.com>
2021-03-04 09:04:33 -0300

    corrected js
    I updated so it now works with the new id's and only opens one question per time

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const answers = document.querySelectorAll(".answer");
 const container = document.querySelector(".container");
 const boxImg = document.querySelector(".box-img")
 const info = document.querySelector(".info");
+let actualQuestion = 1;
 
 /*================
  Event listeners
@@ -14,45 +15,51 @@ const info = document.querySelector(".info");
 //event listener to open the question when enter is touched over the question
 questionsSelector.forEach(question => question.addEventListener('keypress', function (e){
     if (e.key === 'Enter') {
-        showAnswer (question.id, question);
+        console.log(question)
+        showAnswer (question.parentElement.classList[1], question);
     }
 }));
 
 //E.L. to open the question when is clicked
 questionsSelector.forEach(question => {
     question.addEventListener('click', function show() {
-        showAnswer(question.id, question);
+        showAnswer(question.parentElement.classList[1], question);
     });
 });
 
 //E.L to move the container and the box when the .info is hover
 document.addEventListener('mousemove', function checkHover() {
-    questionsSelector.forEach(question => {
-        const hovered = isHover(info);
-        if (hovered !== checkHover.hovered) {
-            checkHover.hovered = hovered;  
-            if (checkHover.hovered == true) {
+    const hovered = isHover(info);
+    if (hovered !== checkHover.hovered) {
+        checkHover.hovered = hovered;  
+        if (checkHover.hovered == true) {
+            container.classList.toggle("move");
+            boxImg.classList.toggle("move");    
+        } else {
+            if (container.classList.length == 3) {
                 container.classList.toggle("move");
                 boxImg.classList.toggle("move");    
-            } else {
-                if (container.classList.length == 3) {
-                    container.classList.toggle("move");
-                    boxImg.classList.toggle("move");    
-                }
             }
         }
-    })
+    }
 });
 
 /*==========
  Functions
 ===========*/
 
-function showAnswer (questionId, questionElement) {
-        answers[questionId - 1].classList.toggle("active");
+function showAnswer (questionPosition, questionElement) {
+    //Desactivate the open answer
+    answers[actualQuestion].classList.toggle("active");
+    questionsSelector[actualQuestion].children[0].classList.toggle("selected");
+    questionsSelector[actualQuestion].children[1].children[0].classList.toggle("selected");
+    //Open the new answer
+    answers[questionPosition - 1].classList.toggle("active");
+    questionElement.children[0].classList.toggle("selected");
+    questionElement.children[1].children[0].classList.toggle("selected");
+    //Assign new answer its position
+    actualQuestion = questionPosition - 1;
 
-        questionElement.children[0].classList.toggle("selected");
-        questionElement.children[1].children[0].classList.toggle("selected");
 }
 
 //Function to check if is hover
